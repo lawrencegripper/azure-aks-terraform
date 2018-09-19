8d11939fed54fdecbc122d3456a1fd45e1137655
Lawrence Gripper <info@grippers.co.uk>
2018-09-19 11:45:03 +0100

    Move to add oms solution in tf for containerInsights

--- oms/oms.tf
+++ oms/oms.tf
@@ -23,9 +23,26 @@ resource "azurerm_log_analytics_workspace" "workspace" {
   name                = "k8s-workspace-${random_id.workspace.hex}"
   location            = "${var.resource_group_location}"
   resource_group_name = "${var.resource_group_name}"
-  sku                 = "Free"
+  sku                 = "Standalone"
+}
+
+
+resource "azurerm_log_analytics_solution" "container_insights" {
+  location              = "${var.resource_group_location}"
+  resource_group_name   = "${var.resource_group_name}"
+  workspace_resource_id = "${azurerm_log_analytics_workspace.workspace.id}"
+  workspace_name        = "${azurerm_log_analytics_workspace.workspace.name}"
+  solution_name         = "ContainerInsights"
+
+  plan {
+    publisher = "Microsoft"
+    product   = "OMSGallery/ContainerInsights"
+  }
 }
 
 output "id" {
   value = "${azurerm_log_analytics_workspace.workspace.id}"
+  depends_on = [
+    "azurerm_log_analytics_solution.container_insights"
+  ]
 }
